63f357de67c4399bd43e5a223337677fbdca3be0
Amr Alaa <n1amr1@gmail.com>
2015-12-05 16:57:30 +0200

    create test seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Lecture.destroy_all
+Slide.destroy_all
+
+Dir.foreach('./data/lectures') do |lecture_file|
+  next if lecture_file == '.' or lecture_file == '..'
+  
+  lecture = Lecture.new(name: lecture_file)
+
+  Dir.foreach('./data/slides/' + lecture_file.split('.')[0]) do |slide_file|
+    next if slide_file == '.' or slide_file == '..'
+    slide = Slide.new(path: '/data/slides/' + lecture_file.split('.')[0] + '/' + slide_file)
+    slide.lecture = lecture
+    slide.save
+  end
+  lecture.save
+end
