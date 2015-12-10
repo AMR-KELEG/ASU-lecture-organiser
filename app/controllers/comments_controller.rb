0793c056be0252e964175899f9e7f7d38b923bd0
thabetx <thabetx@gmail.com>
2015-12-10 21:29:37 +0200

    added the comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+  before_action :set_slide, only: [:create, :destroy]
+  
+  def create
+    @comment = @slide.comments.new(comment_params)
+    if @comment.save
+      redirect_to @slide, notice: "comment successfully added"
+    else
+      redirect_to @slide, alert: "unable to add the comment"
+    end
+  end
+
+  def destroy
+    @comment = @slide.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @slide, notice: "comment deleted!"
+  end
+
+  private
+    def set_slide
+      @slide = Slide.find(params[:slide_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+end
