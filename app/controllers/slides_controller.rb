edd7c5798304097003fcea5aae91bc211f2b2ba9
thabetx <thabetx@gmail.com>
2015-12-15 22:33:45 +0200

    fixed navigating to a slide

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,10 +1,10 @@
 class SlidesController < ApplicationController
+  before_action :set_lecture
   before_action :set_slide, only: [:show, :edit, :update, :destroy]
-
   # GET /slides
   # GET /slides.json
   def index
-    @slides = Slide.all.order(page_number: :asc)
+    @slides = @lecture.slides.all.order(page_number: :asc)
   end
 
   # GET /slides/1
@@ -13,9 +13,9 @@ class SlidesController < ApplicationController
   end
 
   # GET /slides/new
-  def new
-    @slide = Slide.new
-  end
+  # def new
+    # @slide = @lecture.slides.new
+  # end
 
   # GET /slides/1/edit
   def edit
@@ -24,7 +24,7 @@ class SlidesController < ApplicationController
   # POST /slides
   # POST /slides.json
   def create
-    @slide = Slide.new(slide_params)
+    @slide = @lecture.slides.new(slide_params)
 
     respond_to do |format|
       if @slide.save
@@ -39,21 +39,22 @@ class SlidesController < ApplicationController
 
   # PATCH/PUT /slides/1
   # PATCH/PUT /slides/1.json
-  def update
-    respond_to do |format|
-      if @slide.update(slide_params)
-        format.html { redirect_to @slide, notice: 'Slide was successfully updated.' }
-        format.json { render :show, status: :ok, location: @slide }
-      else
-        format.html { render :edit }
-        format.json { render json: @slide.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+    # respond_to do |format|
+      # if @slide.update(slide_params)
+        # format.html { redirect_to @slide, notice: 'Slide was successfully updated.' }
+        # format.json { render :show, status: :ok, location: @slide }
+      # else
+        # format.html { render :edit }
+        # format.json { render json: @slide.errors, status: :unprocessable_entity }
+      # end
+    # end
+  # end
 
   # DELETE /slides/1
   # DELETE /slides/1.json
   def destroy
+    @slide = @lecture.slides.find(params[:id])
     @slide.destroy
     respond_to do |format|
       format.html { redirect_to slides_url, notice: 'Slide was successfully destroyed.' }
@@ -63,8 +64,12 @@ class SlidesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_lecture
+      @lecture = Lecture.find(params[:lecture_id])
+    end
+
     def set_slide
-      @slide = Slide.find(params[:id])
+      @slide = @lecture.slides.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
