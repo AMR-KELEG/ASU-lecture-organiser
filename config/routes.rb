d2b8bcd3ec771e7063ecc099f0363f474d0108c4
Amr Keleg <amr_mohamed@live.com>
2015-12-16 23:49:39 +0200

    Create the sessoins resources and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root 'lectures#index'
 
   resources :slides do
@@ -9,6 +15,9 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new', as: "signup"
   resources :users
 
+  resources :sessions , only: [:new,:create,:destroy]
+  get "/login" => "sessions#new" ,as: "login"
+  delete "/logout" => "sessions#destroy" ,as: "logout"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
