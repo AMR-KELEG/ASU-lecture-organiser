dae1063e8cc0b66842abd7ede53d42a796dcabd1
thabetx <thabetx@gmail.com>
2015-12-16 20:58:03 +0200

    generates random string when lecture names is repeated

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,6 +1,18 @@
 class Lecture < ActiveRecord::Base
   extend FriendlyId
-  friendly_id :name
+  friendly_id :slug_candidates, use: :slugged
+
+  def slug_candidates
+    [
+      :name,
+      [:name, generate_code(3)]
+    ]
+  end
+
+  def generate_code(number)
+    charset = Array('A'..'Z') + Array('a'..'z')+Array(0..9)
+    charset.shuffle[0,3].join
+  end
 
   mount_uploader :attachment, LectureUploader
   validates :name, presence: true
