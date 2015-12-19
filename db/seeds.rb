b3eeba73a236e54e1c98f0d2ae88e5113e6389ea
Amr Alaa <n1amr1@gmail.com>
2015-12-19 16:00:13 +0200

    edit seeds.rb to work with new folder structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,37 +27,43 @@ Comment.destroy_all
 
 random = Random.new(Time.now.to_i)
 
-Dir.foreach('./public/data/lectures') do |lecture_file|
-  next if lecture_file == '.' or lecture_file == '..'
+Dir.foreach('./public/data/lectures') do |lecture_folder|
+  next if lecture_folder == '.' or lecture_folder == '..'
+  
+  Dir.foreach("./public/data/lectures/#{lecture_folder}") do |file|
+    next if file == '.' or file == '..' or file == 'slides' 
 
-  lecture = Lecture.create! name: lecture_file.split('.')[0], path: "/data/lectures/#{lecture_file}"
-  lecture_title = lecture_file.split('.')[0]
+    lecture_title = file.split('.')[0]
+    lecture_path = "/data/lectures/#{lecture_folder}/#{file}"
 
-  pdf = Magick::ImageList.new("./public/data/lectures/#{lecture_file}")
-  counter = 1
-  pdf.each do |page|
-    slide_path = "/data/slides/#{lecture_title}/#{counter}.png"
-    check_dir "public/#{slide_path}"
-    page.write "public/#{slide_path}"
+    lecture = Lecture.create! name: lecture_title, path: lecture_path
 
-    counter += 1
-  end
+    pdf = Magick::ImageList.new("./public/#{lecture_path}")
+    counter = 1
+    pdf.each do |page|
+      slide_path = "/data/lectures/#{lecture_folder}/slides/#{counter}.png"
+      check_dir "public/#{slide_path}"
+      page.write "public/#{slide_path}"
 
-  Dir.foreach("./public/data/slides/#{lecture_title}") do |slide_file|
-    next if slide_file == '.' or slide_file == '..'
-    slide = lecture.slides.create! ({
-      path: "/data/slides/#{lecture_title}/#{slide_file}",
-      page_number: slide_file.split('.')[0].to_i
-      })
+      counter += 1
+    end
 
-    5.times do
-      slide.comments.create! ({
-        text: ('a'..'z').to_a.shuffle[0,20].join,
-        user: User.all.sample,
-        created_at: Time.now + (random.rand * 1e5).seconds,
-        commentable_type: 'Slide',
-        commentable_id: slide.id
+    Dir.foreach("./public/data/lectures/#{lecture_folder}/slides") do |slide_file|
+      next if slide_file == '.' or slide_file == '..'
+      slide = lecture.slides.create! ({
+        path: "/data/lectures/#{lecture_folder}/slides/#{slide_file}",
+        page_number: slide_file.split('.')[0].to_i
         })
+
+      3.times do
+        slide.comments.create! ({
+          text: ('a'..'z').to_a.shuffle[0,20].join,
+          user: User.all.sample,
+          created_at: Time.now + (random.rand * 1e5).seconds,
+          commentable_type: 'Slide',
+          commentable_id: slide.id
+          })
+      end
     end
   end
 end
