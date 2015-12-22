31c8550a6d85f9760e1e3a22bacb2c1b8661fc39
thabetx <thabetx@gmail.com>
2015-12-22 17:28:21 +0200

    added authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :logged_in? , :current_user
+  before_action :ensure_login
 
   protected
+  def ensure_login
+    redirect_to login_path unless session[:user_id]
+  end
+
   def logged_in?
   	session[:user_id]
   end
-  def current_user 
+  def current_user
   	@current_user ||=User.find(session[:user_id])
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
+
+  skip_before_action :ensure_login, only: [:new, :create]
+  before_action :ensure_not_logged_in, only: [:new, :create]
+
   def new
   end
 
@@ -8,14 +12,18 @@ class SessionsController < ApplicationController
 
   	if user && user.authenticate(password)
   		session[:user_id] = user.id
-  		redirect_to root_path ,notice: "You have logged in successfully" 
+  		redirect_to root_path ,notice: "You have logged in successfully"
   	else
   		redirect_to login_path,alert: "Wrong username/password combination"
   	end
   end
 
   def destroy
-  	reset_session 
+  	reset_session
   	redirect_to login_path , notice:"You have been logged out"
+  end
+
+  def ensure_not_logged_in
+    redirect_to root_path if session[:user_id]
   end
 end
