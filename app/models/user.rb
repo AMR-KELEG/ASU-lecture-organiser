53106c87e8ec4b077c45be69746fef709855fb03
Amr Mohamed <amr_mohamed@live.com>
2016-02-10 09:37:27 +0200

    Validate that the password has a min length of 8 chars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :username, uniqueness: true,presence: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+
 end
