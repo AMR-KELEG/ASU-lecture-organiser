93be462a43f94dc0ea508a83348a5bed4e7a25ee
Amr Mohamed <amr_mohamed@live.com>
2016-02-16 23:45:46 +0200

    Remove the presence validation to prevent duplicate error messages on Signuo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :username, uniqueness: true,presence: true
-  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+  validates :password, confirmation: true, length: { minimum: 8 }
 end
